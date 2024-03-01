fc8d26e3baa9584cfe3e4fb82d5aa76190fb4b15
Faisal Hameed <faisalh374@gmail.com>
2024-03-01 20:11:30 +0500

    1assigment

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+//assidment #1
+//stroing the person name ina variable
+let username = 'faisal hameed';
+//printing massage to person
+console.log('Hello , would you like to learn some Python today?');
+console.log(username);
---
+++ app.ts
@@ -0,0 +1,17 @@
+//assidment #1
+
+//stroing the person name ina variable
+
+let username :string ='faisal hameed'
+
+
+
+//printing massage to person
+console.log('Hello , would you like to learn some Python today?')
+
+
+console.log(username)
+
+
+    
+
